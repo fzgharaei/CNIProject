4b442e6bc68cdbf1886d877c4ef8d00bdb7c295e
f_gharae <f_gharae@CATAWBA.ENCS.concordia.ca>
2017-09-30 16:13:20 -0400

    basic modification for Post.java

--- POST/src/Post.java
+++ POST/src/Post.java
@@ -4,13 +4,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.URLEncoder;
 
 public class Post{
-  public static void main(String[] argv) throws Exception {
+  public void post(RequestParameters reqParams) throws Exception {
 	//String data = "{\"id\":1,\"firstName\":\"Liam\",\"age\":22,\"lastName\":\"Marco\"}";
+	
 	String data = "k=1&k=2";
-	Socket socket = new Socket("www.posttestserver.com", 80);
+	InetAddress address = InetAddress.getByName(reqParams.getUrl());
+	Socket socket = new Socket(address, 80);
     //System.out.println(data);
     
     //Socket socket = new Socket("www.httpbin.org", 80);
@@ -20,7 +21,7 @@ public class Post{
     BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
     wr.write("POST " + path + " HTTP/1.0\r\n");
     wr.write("Content-Length: " + data.length() + "\r\n");
-    wr.write("Content-Type: application/x-www-form-urlencoded \r\n");
+    wr.write(reqParams.getHeaderString()+" \r\n");
     wr.write("\r\n");
 
     wr.write(data);
