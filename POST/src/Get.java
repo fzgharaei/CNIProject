fa7cf7d32b39f5d9b457795d4f07720aafc2595c
rahulreddy94 <rahulmudupuri19944@gmail.com>
2017-10-01 21:40:55 -0400

    Get Method generalized

--- POST/src/Get.java
+++ POST/src/Get.java
@@ -5,38 +5,40 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
 
-public class Get {
-	private RequestParameters parameters;
+public class Get {	
 
-	public Get(RequestParameters rp) {
-		parameters = rp;
-	}
-
-	public void get(String url, String headers) throws Exception {
+	public void get(RequestParameters rp) throws Exception {
 
 		// Split the host and get part
 		// get information related to header types
 		// Print verbose
 		try {
-			InetAddress address = InetAddress.getByName(url);
-			Socket s = new Socket(address, 80);
-			PrintWriter pw = new PrintWriter(s.getOutputStream());
+			URL url = new URL(rp.getUrl());
+			String path = url.getFile();
+			String host = url.getHost();
+			InetAddress address = InetAddress.getByName(host);
+		
+			Socket socket = new Socket(address, 80);
+			PrintWriter pw = new PrintWriter(socket.getOutputStream());
+			System.out.println(path);
+			System.out.println(host);
 
-			pw.println("GET /get?course=networking&assignment=1 HTTP/1.1");
-			pw.println("Host: www.httpbin.org");
+			pw.println("GET" + path +"HTTP/1.1");
+			pw.println("Host: " +host);
 			pw.println("");
 			pw.flush();
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			String line;
 			while ((line = br.readLine()) != null) {
-				if(parameters.isVerbose())// handle output
+				if(rp.isVerbose())// handle output
 					System.out.println(line);
-				else if(line.isEmpty() && !parameters.isVerbose()) {
+				else if(line.isEmpty() && !rp.isVerbose()) {
 					while ((line = br.readLine()) != null) {
+						//get headers content type values passed by the user 
 						System.out.println(line);
 					}
-					parameters.verbose = false;
+					rp.verbose = false;
 					break;
 				}
 			}
