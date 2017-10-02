c547d65aac5d89c2923d0ff7c7aae3a151e06383
fzgharaei <gharaeemanesh@gmail.com>
2017-10-01 21:30:24 -0400

    Generalizing post

--- POST/src/Post.java
+++ POST/src/Post.java
@@ -4,36 +4,56 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.URL;
 
 public class Post{
-  public void post(RequestParameters reqParams) throws Exception {
-	//String data = "{\"id\":1,\"firstName\":\"Liam\",\"age\":22,\"lastName\":\"Marco\"}";
+  
+	public void post(RequestParameters reqParams) throws Exception {
 	
-	String data = "k=1&k=2";
-	InetAddress address = InetAddress.getByName(reqParams.getUrl());
-	Socket socket = new Socket(address, 80);
-    //System.out.println(data);
-    
-    //Socket socket = new Socket("www.httpbin.org", 80);
-    //curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://posttestserver.com/post.php
-    
-    String path = "/post.php";
-    BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
-    wr.write("POST " + path + " HTTP/1.0\r\n");
-    wr.write("Content-Length: " + data.length() + "\r\n");
-    wr.write(reqParams.getHeaderString()+" \r\n");
-    wr.write("\r\n");
-
-    wr.write(data);
-    wr.flush();
-
-    BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    String line;
-    while ((line = rd.readLine()) != null) {
-      System.out.println(line);
-    }
-    wr.close();
-    rd.close();
-    socket.close();
-  }
+		String data = reqParams.getData();
+		URL url = new URL(reqParams.getUrl());
+		InetAddress address = InetAddress.getByName(url.getHost());
+		try{
+			Socket socket = new Socket(address, 80);
+		    
+		    BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
+		    wr.write("POST " + url.getFile() + " HTTP/1.0\r\n");
+		    wr.write("Content-Length: " + data.length() + "\r\n");
+		    wr.write(reqParams.getHeaderString()+" \r\n");
+		    wr.write("\r\n");
+		    wr.write(data);
+		    wr.flush();
+		    wr.close();
+		    
+		    
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		    String line;
+			while ((line = rd.readLine()) != null) {
+				if(reqParams.isVerbose())
+					System.out.println(line);
+				else if(line.isEmpty()) {
+					while ((line = rd.readLine()) != null)
+						System.out.println(line);
+					break;
+				}
+			}
+			rd.close();
+		    socket.close();
+		    
+		    }catch (Exception e) {
+				e.printStackTrace();
+			}
+	 }
 }
+
+
+
+
+
+
+
+
+//String data = "{\"id\":1,\"firstName\":\"Liam\",\"age\":22,\"lastName\":\"Marco\"}";
+//System.out.println(data);
+//Socket socket = new Socket("www.httpbin.org", 80);
+//curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://posttestserver.com/post.php
