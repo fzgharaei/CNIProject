8d15bddd9f9dc017b751a5cdcc4b6201640b2ba3
rahulreddy94 <rahulmudupuri19944@gmail.com>
2017-10-14 14:33:56 -0400

    Assignment 2 Initial Status

---
+++ Client/src/Redirect.java
@@ -0,0 +1,20 @@
+/**
+ * This class Performs the HTTP GET Redirect functionality only through sockets.
+ * @author RahulReddy
+ * @since 08/10/2017
+ */
+public class Redirect {
+	Get get;
+
+	public void redirect(RequestParameters request, Get getObject) {
+		System.out.println("********************");
+		System.out.println("Redirection should be done to Location:"+ request.getUrl() + "\r\n");
+		System.out.println("********************");
+
+		try {
+			getObject.get(request);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
