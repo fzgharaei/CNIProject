34b70d0fa2c39f43aeeb13473797cd430904d4bf
fzgharaei <gharaeemanesh@gmail.com>
2017-10-15 23:06:17 -0400

    Http Statuses added as enum class

---
+++ Server/src/HttpStatus.java
@@ -0,0 +1,14 @@
+
+public enum HttpStatus {
+	OK(200,"OK"), NFOUND(404, "Not Found"), NMODIFIED(304, "Not Modified");
+	private int value;
+	private String msg;
+	HttpStatus(int stat, String message){
+		this.value=stat;
+		this.msg = message;
+	}
+	@Override
+	  public String toString() {
+		return "HTTP/1.0 "+ this.value + " " + this.msg;
+	 }
+}
