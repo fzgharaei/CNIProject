85c782c6a3d54cb86ed2cdb4f1476bb73840ca22
rahulreddy94 <rahulmudupuri19944@gmail.com>
2017-10-15 20:28:59 -0400

    Testing connection Client and server

--- Server/src/ResponseManager.java
+++ Server/src/ResponseManager.java
@@ -1,6 +1,19 @@
+import java.util.HashMap;
 
+/**
+ * 
+ * @author RahulReddy
+ *
+ */
 public class ResponseManager {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws Exception{
+		String[] data = {"-v","-p","8080","-d","../"};
+		ServerCmdParser parser = new ServerCmdParser();
+		HashMap<String, String> input = parser.parse(data);
+		System.out.println(Integer.parseInt(input.get("port")));
+		Server server = new Server(Integer.parseInt(input.get("port")), Boolean.parseBoolean(input.get("verbose")), input.get("path"));
+		server.init();
+		server.listen();
+		server.serve();
 	}
 }
